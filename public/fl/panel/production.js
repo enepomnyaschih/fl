eb9fafab5e723595c328765f879739fd48a7ed10
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2015-01-05 01:09:27 +0600

    Improved production presentation.

--- public/fl/panel/production.js
+++ public/fl/panel/production.js
@@ -5,19 +5,42 @@ FL.Panel.Production = function(base) {
 };
 
 JW.extend(FL.Panel.Production, JW.UI.Component, {
-	progressWidth: 200,
+	progressWidth: 150,
 
 	renderProgress: function(el) {
 		el.css("width", this.progressWidth + "px");
+
+		if (!this.base.mining) {
+			return;
+		}
+		var value = this.base.overflow + this.base.production[this.type.id];
+		var offset = this._getOffset(value);
+		el.append('<div class="fl-panel-production-progress-fill" style="width: ' +
+			offset + 'px"></div>');
+		while (value < this.type.cost) {
+			var nextValue = value + this.base.mining;
+			var nextOffset = this._getOffset(nextValue);
+			el.append('<div class="fl-panel-production-progress-fill" style="width: ' +
+				(nextOffset - offset) + 'px"></div>');
+			value = nextValue;
+			offset = nextOffset;
+		}
 	},
 
-	renderProgressFill: function(el) {
-		var production = this.base.overflow + this.base.production[this.type.id];
-		var progress = Math.min(1, production / this.type.cost);
-		el.css("width", Math.floor(this.progressWidth * progress) + "px");
+	renderTurns: function(el) {
+		if (!this.base.mining) {
+			return;
+		}
+		var value = this.base.overflow + this.base.production[this.type.id];
+		var turns = Math.ceil((this.type.cost - value) / this.base.mining);
+		el.text(turns + " turn" + ((turns !== 1) ? "s" : ""));
 	},
 
 	renderUnit: function() {
 		return this.own(new FL.UnitInfo(this.type, this.base.player, true));
+	},
+
+	_getOffset: function(value) {
+		return Math.round(this.progressWidth * Math.min(1, value / this.type.cost));
 	}
 });
