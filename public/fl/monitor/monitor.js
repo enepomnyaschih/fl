ca5391f04349024906a89cf215fa73340b360254
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2015-01-08 14:38:28 +0600

    Added arrows highlighting the currently selected cell.

--- public/fl/monitor/monitor.js
+++ public/fl/monitor/monitor.js
@@ -14,11 +14,17 @@ FL.Monitor = function(data) {
 	this.cells = new FL.Matrix(this.data.map.size);
 	this.unitSelection = [];
 	this.collapsed = false;
+	this.beginTime = new Date().getTime();
 	this.own(this.data.nextPlayerEvent.bind(this.selectNext, this));
 	this.own(this.data.mapUpdateEvent.bind(this.updateMap, this));
+	this.own(new JW.Interval(this._animateArrows, this, 40));
 };
 
 JW.extend(FL.Monitor, JW.UI.Component, {
+	arrowsRotationPeriod: 4000,
+	arrowsScalingPeriod: 1000,
+	arrowsSize: 100,
+
 	renderTurn: function(el) {
 		this.own(new JW.UI.TextUpdater(el, this.data.turn));
 	},
@@ -104,6 +110,8 @@ JW.extend(FL.Monitor, JW.UI.Component, {
 		this.baseExitAttachment.set(null);
 		this.selectNextAnimation.set(null);
 		this.unitSelection = [];
+
+		var animateArrows = this.cellSelect != null;
 		this.cellSelect = ij ? this.data.map.getCell(ij) : null;
 		if (this.cellSelect) {
 			var cellEl = this._getCell(ij);
@@ -129,9 +137,24 @@ JW.extend(FL.Monitor, JW.UI.Component, {
 			this.army.set((this.cellSelect.visible[0] && this.cellSelect.unit) ?
 				new FL.Panel.Unit(this, this.cellSelect.unit) : null);
 			this.panel.set(new FL.Panel(this, this.cellSelect));
+			this.getElement("arrows").css("display", "");
+
+			var xyCell = FL.ijToXy(this.cellSelect.ij);
+			var offset = (FL.cellSize - this.arrowsSize) / 2;
+			var xyArrows = FL.Vector.add(xyCell, [offset, offset]);
+			var properties = {
+				left: Math.round(xyArrows[0]) + "px",
+				top : Math.round(xyArrows[1]) + "px"
+			};
+			if (animateArrows) {
+				this.getElement("arrows").animate(properties, 500, "easeOutQuad");
+			} else {
+				this.getElement("arrows").css(properties);
+			}
 		} else {
 			this.army.set(null);
 			this.panel.set(null);
+			this.getElement("arrows").css("display", "none");
 		}
 	},
 
@@ -367,5 +390,14 @@ JW.extend(FL.Monitor, JW.UI.Component, {
 
 	_isCellAutoSelectable: function(cell) {
 		return (cell != null) && (this._isUnitAutoSelectable(cell.unit) || this._isBaseAutoSelectable(cell.base));
+	},
+
+	_animateArrows: function() {
+		var time = new Date().getTime() - this.beginTime;
+		var rotation = 360 * JW.mod(time / this.arrowsRotationPeriod, 1);
+		var scaling = .75 + .5 * Math.abs(JW.smod(time / this.arrowsScalingPeriod, 1));
+		this.getElement("arrows").css("transform",
+			"rotate(" + Math.round(rotation) + "deg) " +
+			"scale(" + scaling.toFixed(3) + ")");
 	}
 });
